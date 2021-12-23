ada255b8c1ee2fb602612d1c08fa88f0798a89bc
thanhhuy2252000 <lethanhhuy2252000@gmail.com>
2021-12-23 10:29:02 +0700

    up nhung file cua giao dien

---
+++ view/layout/footerpage.php
@@ -0,0 +1,11 @@
+<div class="footer-copyright-area">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="footer-copy-right">
+                    <p>Copyright © 2021 <a href="https://colorlib.com/wp/templates/">Dolce</a> All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ view/layout/mobile-menu.php
@@ -0,0 +1,55 @@
+<div class="mobile-menu-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="mobile-menu">
+                    <nav id="dropdown">
+                        <ul class="mobile-menu-nav">
+                            <li><a data-toggle="collapse" data-target="#Charts" href="#">Home <span class="admin-project-icon nalika-icon nalika-down-arrow"></span></a>
+                                <ul class="collapse dropdown-header-top">
+                                    <li><a href="index.php">Dashboard v.1</a></li>
+                                    <li><a href="product-list.php">Product List</a></li>
+                                    <li><a href="product-edit.php">Product Edit</a></li>
+                                    <li><a href="product-detail.php">Product Detail</a></li>
+                                    <li><a href="product-cart.php">Product Cart</a></li>
+                                </ul>
+                            </li>
+                            <li><a data-toggle="collapse" data-target="#others" href="#">Blog <span class="admin-project-icon nalika-icon nalika-down-arrow"></span></a>
+                                <ul id="others" class="collapse dropdown-header-top">
+                                    <li><a href="blog.php">Blog</a></li>
+                                    <li><a href="blog-details.php">Blog Details</a></li>
+                                </ul>
+                            </li>
+                            <li><a data-toggle="collapse" data-target="#formsmob" href="#">Forms <span class="admin-project-icon nalika-icon nalika-down-arrow"></span></a>
+                                <ul id="formsmob" class="collapse dropdown-header-top">
+                                    <li><a href="basic-form-element.html">Basic Form Elements</a>
+                                    </li>
+                                    <li><a href="advance-form-element.html">Advanced Form Elements</a>
+                                    </li>
+                                    <li><a href="password-meter.html">Password Meter</a>
+                                    </li>
+                                    <li><a href="multi-upload.html">Multi Upload</a>
+                                    </li>
+                                    <li><a href="tinymc.html">Text Editor</a>
+                                    </li>
+                                    <li><a href="dual-list-box.html">Dual List Box</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a data-toggle="collapse" data-target="#Pagemob" href="#">Pages <span class="admin-project-icon nalika-icon nalika-down-arrow"></span></a>
+                                <ul id="Pagemob" class="collapse dropdown-header-top">
+                                    <li><a href="login.php">Login</a>
+                                    </li>
+                                    <li><a href="register.php">Register</a>
+                                    </li>
+                                    <li><a href="password-recovery.php">Password Recovery</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
